149a1a8d2d60a9d7e50a33d2288ebc4178cee83e
otaruit <kimuranatsume5656@gmail.com>
2024-08-07 15:14:00 +0900

    commit

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-# import pandas as pd
-
-# # WikipediaのURL
-# url = 'https://www.city.ebetsu.hokkaido.jp/site/kosodate/72891.html'
-
-# # 日本語版にマッチするテーブルを取得
-# df_list = pd.read_html(url, match='【公立】やよい')
-
-# print(df_list[0])
-
-
-
-
-
-
-
-# import pandas as pd
-
-# # WikipediaのURL
-# url = 'https://www.city.ebetsu.hokkaido.jp/site/kosodate/72891.html'
-
-# # 日本語版にマッチするテーブルを取得
-# df_list = pd.read_html(url, match='【公立】やよい')
-
-# # テーブルが見つかった場合、最初のテーブルを取得
-# if df_list:
-#     df = df_list[0]
-    
-#     # 各行を表示
-#     for index, row in df.iterrows():
-#         print(row)
-# else:
-#     print("指定したテーブルが見つかりませんでした。")
 
 import pandas as pd
 
